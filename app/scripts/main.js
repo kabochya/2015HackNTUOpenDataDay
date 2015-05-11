2f4dbc9997dcba69d82d466c19efea6331d1a45c
kabochya <pml0415@gmail.com>
2015-05-11 12:09:53 +0800

    correct DOIT name

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,9 +9,9 @@ var map = new GMaps({
   map.addMarker({
     lat:25.0221324,
     lng:121.547979,
-    title:"DO!T 共創公域",
+    title:"DO!T TAIWAN 共創公域",
     infoWindow: {
-      content: '<p style="color:#999;">DO!T 共創公域</p>'
+      content: '<p style="color:#999;">DO!T TAIWAN 共創公域</p>'
     }
   });
 $(function(){
